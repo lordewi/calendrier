f9903d4cc68105074ba0953f9c7c7dfbd3202532
lordewi <abderahmen.ghorbel@gmail.com>
2025-05-26 16:26:24 +0100

    adding a webhook

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from the API! tes2t" });
 });
 
+app.post('/webhook', (req, res) => {
+    console.log('Webhook received:', req.body);
+    res.status(200).send('OK');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
